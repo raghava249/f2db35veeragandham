6ea6c77d9c4a471749d44190757fd7ec917a3282
raghava249 <111784607+raghava249@users.noreply.github.com>
2022-11-11 11:40:23 -0600

    added files

--- controllers/Employee.js
+++ controllers/Employee.js
@@ -13,9 +13,18 @@ exports.Employee_list = async function(req, res) {
 }; 
  
 // for a specific Employee. 
-exports.Employee_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Employee detail: ' + req.params.id); 
+// for a specific Costume. 
+exports.Employee_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Employee.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
+ 
  
 // Handle Employee create on POST. 
 exports.Employee_create_post = function(req, res) { 
@@ -27,10 +36,27 @@ exports.Employee_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Employee delete DELETE ' + req.params.id); 
 }; 
  
-// Handle Employee update form on PUT. 
-exports.Employee_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Employee update PUT' + req.params.id); 
+exports.Employee_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Employee.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.Employee_Name)  
+               toUpdate.Employee_Name = req.body.Employee_Name; 
+        if(req.body.Employee_Salary) toUpdate.Employee_Salary = req.body.Employee_Salary; 
+        if(req.body.Employee_Id) toUpdate.Employee_Id = req.body.Employee_Id; 
+        if(req.body.Employee_Department) toUpdate.Employee_Department = req.body.Employee_Department; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
+ 
 // VIEWS 
 // Handle a show all view 
 exports.Employee_view_all_Page = async function(req, res) { 
@@ -50,7 +76,7 @@ exports.Employee_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"Employee_Name":"Vidya", "Employee_Salary":100000, "Employee_Id": 67422,Employee_Department:"CEO"} 
+    // {"Employee_Name":"Vidya", "Employee_Salary":100000, "Employee_Id": 67422,"Employee_Department":"CEO"} 
     document.Employee_Name = req.body.Employee_Name; 
     document.Employee_Salary = req.body.Employee_Salary; 
     document.Employee_Id = req.body.Employee_Id; 
