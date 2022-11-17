dd2b80181db294217a9746fe60f8033a503bda0f
raghava249 <111784607+raghava249@users.noreply.github.com>
2022-11-17 17:57:03 -0600

    lab 14

--- controllers/Employee.js
+++ controllers/Employee.js
@@ -103,4 +103,54 @@ exports.Employee_delete = async function(req, res) {
         res.send(`{"error": Error deleting ${err}}`); 
     } 
 }; 
- 
+
+// Handle a show one view with id specified by query 
+exports.Employee_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Employee.findById( req.query.id) 
+        res.render('Employeedetail',  { title: 'Employee Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ // Handle building the view for creating a Employee. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.Employee_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('Employeecreate', { title: 'Employee Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+// Handle building the view for updating a Employee. 
+// query provides the id 
+exports.Employee_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Employee.findById(req.query.id) 
+        res.render('Employeeupdate', { title: 'Employee Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+// Handle a delete one view with id from query 
+exports.Employee_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Employee.findById(req.query.id) 
+        res.render('Employeedelete', { title: 'Employee Delete', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/Employee.js
+++ routes/Employee.js
@@ -3,5 +3,14 @@ var router = express.Router();
 const Employee_controlers = require('../controllers/Employee');
 /* GET home page. */
 router.get('/',Employee_controlers.Employee_view_all_Page);
+/* GET detail Employee page */ 
+router.get('/detail', Employee_controlers.Employee_view_one_Page); 
+/* GET create Employee page */ 
+router.get('/create', Employee_controlers.Employee_create_Page);
+/* GET create update page */ 
+router.get('/update', Employee_controlers.Employee_update_Page);  
+/* GET delete Employee page */ 
+router.get('/delete', Employee_controlers.Employee_delete_Page); 
+ 
 
 module.exports = router;
