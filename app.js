f89711447af39ba8c81b492734ba89fec9148b17
raghava249 <111784607+raghava249@users.noreply.github.com>
2022-11-17 18:11:29 -0600

    lab 14 completed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
 var Employee = require("./models/Employee"); 
 
 require('dotenv').config(); 
@@ -37,6 +39,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -45,6 +54,15 @@ app.use('/Employee',EmployeeRouter);
 app.use('/gridbulid',gridbulidRouter);
 app.use('/selector',selectorRouter);
 app.use('/resource',resourceRouter);
+
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
